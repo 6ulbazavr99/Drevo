0f2da27fcd474f80603952aa24e1db2f8aba9c29
bulbazavr <6ulbazavr99@gmail.com>
2024-02-25 01:41:15 +0600

    user admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,8 +1,43 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .models import CustomUser
+from django.utils.translation import gettext_lazy as _
 
 
-User = get_user_model()
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = '__all__'
 
 
-admin.site.register(User)
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        fields = '__all__'
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('email', 'first_name', 'last_name', 'gender', 'city', 'country', 'is_active', 'is_staff')
+    list_filter = ('gender', 'city', 'country', 'is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Персональная информация'), {'fields': ('first_name', 'last_name', 'gender', 'birthdate', 'patronymic', 'phone', 'avatar')}),
+        (_('Местоположение'), {'fields': ('birthplace', 'city', 'country')}),
+        (_('Права доступа'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Важные даты'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'is_active', 'is_staff')}
+        ),
+    )
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,15 +1,17 @@
+from django.utils.translation import gettext_lazy as _
+import datetime
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
-
 from apps.users.managers import CustomUserManager
 
 
 class CustomUser(AbstractUser):
-    username = models.CharField(max_length=150, unique=True, blank=True, null=True)
-    email = models.EmailField(unique=True)
-    phone = PhoneNumberField(blank=True, null=True, unique=True)
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    username = models.CharField(_('Имя пользователя'), max_length=150, unique=True, blank=True, null=True)
+    email = models.EmailField(_('Электронная почта'), unique=True)
+    phone = PhoneNumberField(_('Телефон'), blank=True, null=True, unique=True)
+    avatar = models.ImageField(_('Аватар'), upload_to='avatars/', blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -17,37 +19,39 @@ class CustomUser(AbstractUser):
     objects = CustomUserManager()
 
     GENDER_CHOICES = [
-        ('male', 'Мужской'),
-        ('female', 'Женский')
+        ('male', _('Мужской')),
+        ('female', _('Женский'))
     ]
 
-    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True, null=True)
-    patronymic = models.CharField(max_length=255, blank=True, null=True)
-    birthdate = models.DateField(blank=True, null=True)
-    age = models.IntegerField(blank=True, null=True)
+    gender = models.CharField(_('Пол'), max_length=10, choices=GENDER_CHOICES, blank=True, null=True)
+    patronymic = models.CharField(_('Отчество'), max_length=255, blank=True, null=True)
+    birthdate = models.DateField(_('Дата рождения'), blank=True, null=True)
+    age = models.IntegerField(_('Возраст'), blank=True, null=True)
 
-    birthplace = models.CharField(max_length=255, blank=True, null=True)
-    city = models.CharField(max_length=255, blank=True, null=True)
-    country = models.CharField(max_length=255, blank=True, null=True)
+    birthplace = models.CharField(_('Место рождения'), max_length=255, blank=True, null=True)
+    city = models.CharField(_('Город'), max_length=255, blank=True, null=True)
+    country = models.CharField(_('Страна'), max_length=255, blank=True, null=True)
 
-    marriage = models.BooleanField(default=False, blank=True, null=True)
+    marriage = models.BooleanField(_('Брак'), default=False, blank=True, null=True)
     partner = models.ForeignKey('CustomUser', on_delete=models.SET_NULL, null=True,
-                                related_name='partners', blank=True)
+                                related_name='partners', blank=True, verbose_name=_('Партнер'))
 
-    about_me = models.TextField(blank=True, null=True)
-    education = models.CharField(max_length=255, blank=True, null=True)
-    work = models.CharField(max_length=255, blank=True, null=True)
-    images = models.ImageField(upload_to='images/', blank=True, null=True)
-    alive = models.BooleanField(default=True, blank=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    about_me = models.TextField(_('О себе'), blank=True, null=True)
+    education = models.CharField(_('Образование'), max_length=255, blank=True, null=True)
+    work = models.CharField(_('Работа'), max_length=255, blank=True, null=True)
+    images = models.ImageField(_('Изображения'), upload_to='images/', blank=True, null=True)
+    alive = models.BooleanField(_('Жив'), default=True, blank=True, null=True)
+    updated_at = models.DateTimeField(_('Обновлено'), auto_now=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} [{self.email}]'
 
     def save(self, *args, **kwargs):
+        if self.birthdate:
+            today = datetime.date.today()
+            self.age = today.year - self.birthdate.year - (
+                    (today.month, today.day) < (self.birthdate.month, self.birthdate.day))
+
         if not self.username:
             self.username = self.email.split('@')[0] + str(CustomUser.objects.count())
         super().save(*args, **kwargs)
-
-
-# TODO: FIX THE PASSWORD WHEN CREATING A REGULAR USER IN THE ADMIN PANEL !!!

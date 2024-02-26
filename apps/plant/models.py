c5c2c78e7cffa98aef01dfa344a357b140057297
Ubuntu <ubuntu@ip-172-31-45-173.eu-north-1.compute.internal>
2024-02-26 06:08:37 +0000

    fix multichoice

--- apps/plant/models.py
+++ apps/plant/models.py
@@ -1,20 +1,21 @@
 from django.contrib.auth import get_user_model
-
 from apps.family.models import FamilyMember
 from django.db import models
 from multiselectfield import MultiSelectField
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
-
 User = get_user_model()
-
 
 def validate_max_choices(value):
     max_choices = 4
     if len(value) > max_choices:
         raise ValidationError(_('Максимальное количество выбранных значений: %(max_choices)s') % {'max_choices': max_choices})
 
+class CustomMultiSelectField(MultiSelectField):
+    def _get_flatchoices(self):
+        # Возвращаем пустой список, имитируя закомментированный метод
+        return []
 
 class PlantedTree(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='planted_tree',
@@ -28,8 +29,8 @@ class PlantedTree(models.Model):
         ('medium', _('Средне')),
         ('bad', _('Плохо')),
     ]
-    condition = MultiSelectField(choices=CONDITION_CHOICES, verbose_name=_("Состояние"), blank=True, null=True,
-                                 validators=[validate_max_choices], default='good')
+    condition = CustomMultiSelectField(choices=CONDITION_CHOICES, verbose_name=_("Состояние"), blank=True, null=True,
+                                       validators=[validate_max_choices], default='good')
 
     type = models.CharField(max_length=255, verbose_name=_("Вид"), blank=True, null=True)
     age = models.IntegerField(_('Возраст'), blank=True, null=True)
@@ -47,19 +48,3 @@ class PlantedTree(models.Model):
     class Meta:
         verbose_name = _("Посаженное дерево")
         verbose_name_plural = _("Посаженные деревья")
-
-
-# TODO: It is necessary to comment on the django 5x
-# MultiSelectField:
-    # def _get_flatchoices(self):
-    #     flat_choices = super(MultiSelectField, self)._get_flatchoices()
-    #
-    #     class MSFFlatchoices(list):
-    #         # Used to trick django.contrib.admin.utils.display_for_field into
-    #         # not treating the list of values as a dictionary key (which errors
-    #         # out)
-    #         def __bool__(self):
-    #             return False
-    #         __nonzero__ = __bool__
-    #     return MSFFlatchoices(flat_choices)
-    # flatchoices = property(_get_flatchoices)
